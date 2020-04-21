1decf174b851a4e143d07783cd69c8ad9cf76026
Maruo110 <32796229+Maruo110@users.noreply.github.com>
2020-04-21 21:18:18 +0900

    整備

--- src_sample/beautiful_soup/web_beautifulsoup4.py
+++ src_sample/beautiful_soup/web_beautifulsoup4.py
@@ -5,20 +5,20 @@ def getWebInfoBeautifulSoup4():
 
     import requests
     from bs4 import BeautifulSoup
+    from util import utils
 
     url = "https://news.yahoo.co.jp/byline/fujitatakanori/20200420-00174220/"
     res = requests.get(url).text
 
     soup = BeautifulSoup(res, 'html.parser') #2
-    #print (soup)
-    #print(soup.prettify())
     ptag = soup.find_all("p")
-    #print(soup.find_all("p"))
+    ptag_value = ""
 
     for p in ptag:
-        #print(type(p))
-        print(p.text)
+        ptag_value = ptag_value + p.text
 
+    ptag_value = utils.removeKaigyou(ptag_value)
+    print(ptag_value)
     print('--------------------------------------------------')
 
 if __name__ == '__main__':
