c08c65fbdb860bfb0097e3615508cea626cecb41
Maruo110 <32796229+Maruo110@users.noreply.github.com>
2020-04-22 13:52:41 +0900

    エンティティ分析（GCP）のサンプルを追加

---
+++ src_sample/google_cloud_api/analyze_entities.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+
+def run_quickstart(text_content):
+    from google.cloud import language_v1
+    from google.cloud.language_v1 import enums
+
+    client = language_v1.LanguageServiceClient()
+
+    # text_content = 'California is a state.'
+
+    # Available types: PLAIN_TEXT, HTML
+    type_ = enums.Document.Type.PLAIN_TEXT
+
+    # Optional. If not specified, the language is automatically detected.
+    # For list of supported languages:
+    # https://cloud.google.com/natural-language/docs/languages
+    language = "ja"
+    document = {"content": text_content, "type": type_, "language": language}
+
+    # Available values: NONE, UTF8, UTF16, UTF32
+    encoding_type = enums.EncodingType.UTF8
+
+    response = client.analyze_entities(document, encoding_type=encoding_type)
+    # Loop through entitites returned from the API
+    for entity in response.entities:
+        print(u"Representative name for the entity: {}".format(entity.name))
+        # Get entity type, e.g. PERSON, LOCATION, ADDRESS, NUMBER, et al
+        print(u"Entity type: {}".format(enums.Entity.Type(entity.type).name))
+        # Get the salience score associated with the entity in the [0, 1.0] range
+        print(u"Salience score: {}".format(entity.salience))
+        # Loop over the metadata associated with entity. For many known entities,
+        # the metadata is a Wikipedia URL (wikipedia_url) and Knowledge Graph MID (mid).
+        # Some entity types may have additional metadata, e.g. ADDRESS entities
+        # may have metadata for the address street_name, postal_code, et al.
+        for metadata_name, metadata_value in entity.metadata.items():
+            print(u"{}: {}".format(metadata_name, metadata_value))
+
+        # Loop over the mentions of this entity in the input document.
+        # The API currently supports proper noun mentions.
+        for mention in entity.mentions:
+            print(u"Mention text: {}".format(mention.text.content))
+            # Get the mention type, e.g. PROPER for proper noun
+            print(
+                u"Mention type: {}".format(enums.EntityMention.Type(mention.type).name)
+            )
+
+    # Get the language of the text, which will be the same as
+    # the language specified in the request or, if not specified,
+    # the automatically-detected language.
+    print(u"Language of the text: {}".format(response.language))
+
+
+
+if __name__ == '__main__':
+    str = "徳島市の阿波踊り中止へ"
+    run_quickstart(str)
